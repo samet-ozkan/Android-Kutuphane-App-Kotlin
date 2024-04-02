8a54e9b60f6d8af5172f8a0b590f0050fc17c5f2
Samet Özkan <samet-ozkan@outlook.com>
2024-04-02 19:09:11 +0300

    Baslangic ayarlandi

---
+++ app/src/main/java/com/sametozkan/kutuphane/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sametozkan.kutuphane
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ornek)
+    }
+}
---
+++ app/src/main/java/com/sametozkan/kutuphane/MyApplication.kt
@@ -0,0 +1,8 @@
+package com.sametozkan.kutuphane
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+}
